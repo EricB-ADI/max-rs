1d996c729014291dda052531c0c1b76e752a62f0
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2024-03-08 09:48:48 -0600

    added impl of gpio for port 1 & 2

--- max32655/max32655-hal/src/gpio.rs
+++ max32655/max32655-hal/src/gpio.rs
@@ -64,9 +64,6 @@ impl GpioPort for pac::GPIO0 {
         if en {
             self.en0_set.write(|w| unsafe { w.bits(mask) });
             self.inen.modify(|r, w| unsafe { w.bits(r.bits() | mask) });
-
-            assert!(self.en0.read().bits() & mask != 0);
-            assert!(self.inen.read().bits() & mask != 0);
         } else {
             self.en0_clr.write(|w| unsafe { w.bits(mask) });
             self.inen.modify(|r, w| unsafe { w.bits(r.bits() & !mask) });
@@ -78,8 +75,157 @@ impl GpioPort for pac::GPIO0 {
         self.padctrl0
             .modify(|r, w| unsafe { w.bits(r.bits() | mask) });
 
-        assert!(self.padctrl0.read().bits() & mask != 0);
-        assert!(self.padctrl1.read().bits() & mask == 0);
+        Ok(())
+    }
+    fn set_high(&mut self, pin: u8) -> Result<(), GpioError> {
+        if pin > 31 {
+            return Err(GpioError::InvalidPin);
+        }
+
+        self.out_set.write(|w| unsafe { w.bits(1 << pin) });
+
+        Ok(())
+    }
+    fn set_low(&mut self, pin: u8) -> Result<(), GpioError> {
+        if pin > 31 {
+            return Err(GpioError::InvalidPin);
+        }
+
+        self.out_clr.write(|w| unsafe { w.bits(1 << pin) });
+
+        Ok(())
+    }
+
+    fn read(&mut self, pin: u8) -> Result<bool, GpioError> {
+        if pin > 31 {
+            return Err(GpioError::InvalidPin);
+        }
+
+        let input = (self.in_.read().bits() >> pin) & 1;
+
+        Ok(input != 0)
+    }
+}
+
+impl GpioPort for pac::GPIO1 {
+    fn enable_output(&mut self, en: bool, pin: u8) -> Result<(), GpioError> {
+        if pin > 31 {
+            return Err(GpioError::InvalidPin);
+        }
+
+        if en {
+            self.en1_set.write(|w| unsafe { w.bits(1 << pin) });
+            self.outen_set.write(|w| unsafe { w.bits(1 << pin) });
+        } else {
+            self.en1_clr.write(|w| unsafe { w.bits(1 << pin) });
+            self.outen_clr.write(|w| unsafe { w.bits(1 << pin) });
+        }
+
+        self.padctrl0
+            .modify(|r, w| unsafe { w.bits(r.bits() & !(1 << pin)) });
+
+        self.padctrl1
+            .modify(|r, w| unsafe { w.bits(r.bits() & !(1 << pin)) });
+
+        Ok(())
+    }
+
+    fn enable_input(&mut self, en: bool, pin: u8) -> Result<(), GpioError> {
+        if pin > 31 {
+            return Err(GpioError::InvalidPin);
+        }
+
+        let mask = 1 << pin;
+
+        if en {
+            self.en1_set.write(|w| unsafe { w.bits(mask) });
+            self.inen.modify(|r, w| unsafe { w.bits(r.bits() | mask) });
+        } else {
+            self.en1_clr.write(|w| unsafe { w.bits(mask) });
+            self.inen.modify(|r, w| unsafe { w.bits(r.bits() & !mask) });
+        }
+
+        self.padctrl1
+            .modify(|r, w| unsafe { w.bits(r.bits() & !mask) });
+
+        self.padctrl0
+            .modify(|r, w| unsafe { w.bits(r.bits() | mask) });
+
+        Ok(())
+    }
+    fn set_high(&mut self, pin: u8) -> Result<(), GpioError> {
+        if pin > 31 {
+            return Err(GpioError::InvalidPin);
+        }
+
+        self.out_set.write(|w| unsafe { w.bits(1 << pin) });
+
+        Ok(())
+    }
+    fn set_low(&mut self, pin: u8) -> Result<(), GpioError> {
+        if pin > 31 {
+            return Err(GpioError::InvalidPin);
+        }
+
+        self.out_clr.write(|w| unsafe { w.bits(1 << pin) });
+
+        Ok(())
+    }
+
+    fn read(&mut self, pin: u8) -> Result<bool, GpioError> {
+        if pin > 31 {
+            return Err(GpioError::InvalidPin);
+        }
+
+        let input = (self.in_.read().bits() >> pin) & 1;
+
+        Ok(input != 0)
+    }
+}
+
+impl GpioPort for pac::GPIO2 {
+    fn enable_output(&mut self, en: bool, pin: u8) -> Result<(), GpioError> {
+        if pin > 31 {
+            return Err(GpioError::InvalidPin);
+        }
+
+        if en {
+            self.en2_set.write(|w| unsafe { w.bits(1 << pin) });
+            self.outen_set.write(|w| unsafe { w.bits(1 << pin) });
+        } else {
+            self.en2_clr.write(|w| unsafe { w.bits(1 << pin) });
+            self.outen_clr.write(|w| unsafe { w.bits(1 << pin) });
+        }
+
+        self.padctrl0
+            .modify(|r, w| unsafe { w.bits(r.bits() & !(1 << pin)) });
+
+        self.padctrl1
+            .modify(|r, w| unsafe { w.bits(r.bits() & !(1 << pin)) });
+
+        Ok(())
+    }
+
+    fn enable_input(&mut self, en: bool, pin: u8) -> Result<(), GpioError> {
+        if pin > 31 {
+            return Err(GpioError::InvalidPin);
+        }
+
+        let mask = 1 << pin;
+
+        if en {
+            self.en2_set.write(|w| unsafe { w.bits(mask) });
+            self.inen.modify(|r, w| unsafe { w.bits(r.bits() | mask) });
+        } else {
+            self.en2_clr.write(|w| unsafe { w.bits(mask) });
+            self.inen.modify(|r, w| unsafe { w.bits(r.bits() & !mask) });
+        }
+
+        self.padctrl1
+            .modify(|r, w| unsafe { w.bits(r.bits() & !mask) });
+
+        self.padctrl0
+            .modify(|r, w| unsafe { w.bits(r.bits() | mask) });
 
         Ok(())
     }

cc1b083c286e8e43d197dd6f1e1c6516a7bf7879
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2024-03-11 13:40:13 -0500

    adc basics

--- max32655/max32655-hal/src/adc.rs
+++ max32655/max32655-hal/src/adc.rs
@@ -1,6 +1,10 @@
 use crate::pac;
 use crate::Sys;
 
+pub enum AdcError {
+    Overflow,
+}
+
 pub enum Chan {
     AIN0,
     AIN1,
@@ -15,10 +19,16 @@ pub enum Chan {
     VRXOUT,
     VTXOUT,
     VDDDA,
-    VDDIO4,
+    VDDIO4 = 14,
     VDDIOH4,
     VREGI4,
 }
+
+pub enum DataAlignment {
+    LSB,
+    MSB,
+}
+
 pub enum RefSel {
     Bandgap,
     VDD2,
@@ -29,16 +39,30 @@ pub enum AdcFlags {}
 pub trait Adc {
     fn init(&mut self);
     fn shutdown(&mut self);
-    fn is_busy(&mut self);
+    fn is_busy(&mut self) -> bool;
     fn enable_interrupt(&mut self);
-    fn get_flags(&mut self) -> u32;
+    fn get_flags(&mut self) -> u16;
     fn clear_flags(&mut self, flags: u32);
     fn set_conversion_speed(&mut self, hz: u32);
     fn get_conversion_speed(&mut self) -> u32;
-    fn set_data_alignment(&mut self, msb_justify: i32);
+    fn set_data_alignment(&mut self, alignment: DataAlignment);
+    fn get_data_alignment(&self) -> DataAlignment;
     fn set_ext_scale(&mut self);
-    fn ref_select(&mut self, refsel: RefSel);
-    fn read(&mut self, channel: Chan) -> u16;
+    fn set_reference(&mut self, refsel: RefSel);
+    fn set_channel(&mut self, channel: Chan);
+    fn start_conversion(&self);
+    fn get_conversion(&self) -> Result<u16, AdcError>;
+
+    fn wait_until_not_busy(&mut self) {
+        while self.is_busy() {}
+    }
+    fn read_sync(&mut self, channel: Chan) -> Result<u16, AdcError> {
+        self.set_channel(channel);
+        self.start_conversion();
+        self.wait_until_not_busy();
+
+        self.get_conversion()
+    }
 }
 
 impl Adc for pac::ADC {
@@ -63,10 +87,12 @@ impl Adc for pac::ADC {
         self.clear_flags(ref_ready_bit);
     }
     fn shutdown(&mut self) {}
-    fn is_busy(&mut self) {}
+    fn is_busy(&mut self) -> bool {
+        self.status.read().active().bit_is_set() && self.status.read().overflow().bit_is_clear()
+    }
     fn enable_interrupt(&mut self) {}
-    fn get_flags(&mut self) -> u32 {
-        0
+    fn get_flags(&mut self) -> u16 {
+        ((self.intr.read().bits() >> 16) & 0xFFFF) as u16
     }
     fn clear_flags(&mut self, flags: u32) {
         self.intr.modify(|r, w| unsafe { w.bits(r.bits() | flags) });
@@ -79,23 +105,50 @@ impl Adc for pac::ADC {
     fn get_conversion_speed(&mut self) -> u32 {
         0
     }
-    fn set_data_alignment(&mut self, msb_justify: i32) {
-        let _ = msb_justify;
-        todo!();
+
+    fn get_data_alignment(&self) -> DataAlignment {
+        if self.ctrl.read().data_align().bit_is_set() {
+            DataAlignment::MSB
+        } else {
+            DataAlignment::LSB
+        }
     }
-    fn set_ext_scale(&mut self) {}
-    fn ref_select(&mut self, refsel: RefSel) {
-        self.ctrl.modify(|r, w| unsafe {
-            let refsel_mask = 1 << 4;
-            match refsel {
-                RefSel::Bandgap => w.bits(r.bits() & !refsel_mask),
-                RefSel::VDD2 => w.bits(r.bits() | refsel_mask),
-            }
+
+    fn set_data_alignment(&mut self, alignment: DataAlignment) {
+        match alignment {
+            DataAlignment::LSB => self.ctrl.write(|w| w.data_align().clear_bit()),
+            DataAlignment::MSB => self.ctrl.write(|w| w.data_align().set_bit()),
+        }
+    }
+
+    fn set_ext_scale(&mut self) {
+        todo!()
+    }
+    fn set_reference(&mut self, refsel: RefSel) {
+        self.ctrl.write(|w| match refsel {
+            RefSel::Bandgap => w.ref_sel().clear_bit(),
+            RefSel::VDD2 => w.ref_sel().set_bit(),
         });
     }
-    fn read(&mut self, channel: Chan) -> u16 {
-        let _ = channel;
+    fn set_channel(&mut self, channel: Chan) {
+        self.ctrl.write(|w| unsafe {
+            w.ch_sel().bits(0);
+            w.ch_sel().bits(channel as u8)
+        });
+    }
+    fn start_conversion(&self) {
+        self.ctrl.write(|w| w.start().set_bit());
+    }
+    fn get_conversion(&self) -> Result<u16, AdcError> {
+        if self.status.read().overflow().bit_is_set() {
+            return Err(AdcError::Overflow);
+        }
 
-        self.data.read().bits() as u16
+        let data = self.data.read().bits() as u16;
+
+        match self.get_data_alignment() {
+            DataAlignment::LSB => Ok(data),
+            DataAlignment::MSB => Ok(data >> 5),
+        }
     }
 }
